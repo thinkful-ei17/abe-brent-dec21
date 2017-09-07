9d61103fb0486abc422375d92d5a4f08535b22e3
Rich Greenhill <rgnet1@yahoo.com>
2017-09-07 15:02:11 -0700

    first

---
+++ spotify.js
@@ -0,0 +1,53 @@
+// !!!FILL IN YOUR CLIENT ID FROM YOUR APPLICATION CONSOLE:
+// https://developer.spotify.com/my-applications/#!/applications !!!
+const CLIENT_ID = 'YOUR_ID_HERE';
+
+const getFromApi = function (endpoint, query = {}) {
+  // You won't need to change anything in this function, but you will use this function to make
+  // calls to Spotify's API.
+
+  const url = new URL(`https://api.spotify.com/v1/${endpoint}`);
+  const headers = new Headers();
+  headers.set('Authorization', `Bearer ${localStorage.getItem('SPOTIFY_ACCESS_TOKEN')}`);
+  headers.set('Content-Type', 'application/json');
+  const requestObject = {
+    headers
+  };
+
+  Object.keys(query).forEach(key => url.searchParams.append(key, query[key]));
+  return fetch(url, requestObject).then(function (response) {
+    if (!response.ok) {
+      return Promise.reject(response.statusText);
+    }
+    return response.json();
+  });
+};
+
+let artist;
+
+const getArtist = function (name) {
+};
+
+
+
+
+
+// =========================================================================================================
+// IGNORE BELOW THIS LINE - THIS IS RELATED TO SPOTIFY AUTHENTICATION AND IS NOT NECESSARY  
+// TO REVIEW FOR THIS EXERCISE
+// =========================================================================================================
+const login = function () {
+  const AUTH_REQUEST_URL = 'https://accounts.spotify.com/authorize';
+  const REDIRECT_URI = 'http://localhost:8080/auth.html';
+
+  const query = new URLSearchParams();
+  query.set('client_id', CLIENT_ID);
+  query.set('response_type', 'token');
+  query.set('redirect_uri', REDIRECT_URI);
+
+  window.location = AUTH_REQUEST_URL + '?' + query.toString();
+};
+
+$(() => {
+  $('#login').click(login);
+});
